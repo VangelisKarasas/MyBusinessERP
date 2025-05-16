1db376f04e67f6fc4e1a14deb0b6a9396eaf1bde
VangelisKarasas <120601673+VangelisKarasas@users.noreply.github.com>
2025-05-16 22:21:18 +0300

    Update routes.py

--- customertracking/routes.py
+++ customertracking/routes.py
@@ -3,70 +3,12 @@ from customertracking import app, db, bcrypt
 from customertracking.forms import RegistrationForm, LoginForm, DocumentForm
 from customertracking.models import User, Customer
 from flask_login import login_user, current_user, logout_user, login_required
+from customertracking.dbtest import customers, tasks, items, last_sales
 
-customers = [
-    {
-        'Name': 'Κώστας Παπαδημητρίου',
-        'Balance': 1.345,
-        'Address': 'Καποδιστρίου 64'
-    },
-    {
-        'Name': 'Γιώργος Παπαβασιλείου',
-        'Balance': 10.000,
-        'Address': 'Χατζηζωγίδου 23'
-    }
-]
-
-tasks = [
-    {
-        'id': '1',
-        'Name': 'Κώστας Παπαδημητρίου',
-        'Description': 'Τοποθέτηση μαρμάρων 30μ',
-        'Cost': '1200',
-        'Completion_date': '5/5/2025'
-    },
-    {
-        'id': '2',
-        'Name': 'Γιώργος Αντωνίου',
-        'Description': 'Τοποθέτηση πλακάκια 30 τ.μ',
-        'Cost': '1000',
-        'Completion_date': '2/5/2025'
-    }
-]
-
-items = [
-    {
-        'Id': '1',
-        'Brand': 'Kraft',
-        'Description': 'Τρυπάνι Δυνατό',
-        'Cost': 130,
-        'Size': 'Large'
-    },
-    {
-        'Id': '2',
-        'Brand': 'FF Group',
-        'Description': 'Γάντια Αντοχής',
-        'Cost': 1.20,
-        'Size': 'S'
-    }
-]
-
-last_sales = [{
-    'id': 1,
-    'Document_code': 'ΤΔΑ-Α-000001',
-    'Document_date': '01-01-2025',
-    'Total_amount': 1240,
-    'Net_value': 1000,
-    'Vat_amount': 240
-},
-    {
-    'id': 2,
-    'Document_code': 'ΤΔΑ-Α-000002',
-    'Document_date': '01-02-2025',
-    'Total_amount': 124,
-    'Net_value': 100,
-    'Vat_amount': 24
-}]
+customers = customers
+tasks = tasks
+items = items
+last_sales = last_sales
 
 
 @app.route("/")
